7327e867b10f707bfbc2e7c02b19b506c4343e7d
Nathaniel <nathaniel@fightthecurrent.org>
2015-06-12 12:17:55 -0500

    Update protagonist.js
    Made async again.

--- tasks/protagonist.js
+++ tasks/protagonist.js
@@ -15,41 +15,50 @@ module.exports = function(grunt) {
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
-  function parseAPI(filepath) {
+  function parseAPI(filepath, done) {
     if (!grunt.file.exists(filepath)) {
       grunt.fail.warn('File ' + filepath + ' doesn\'t exist')
-      return false;
+        return false;
     }
 
-    var api = fs.readFileSync(filepath, 'utf8'),
-        result = protagonist.parseSync(api);
+    var api = fs.readFileSync(filepath, 'utf8');
 
-    if (result.warnings.length > 0) {
-      var warnings = '';
-      result.warnings.forEach(function (item, i, a) {
-        item.location.forEach(function (loc, k, b) {
-          warnings += 'line:' + api.substr(0, loc.index).split('\n').length + ' - ' + item.message + "\n";
+    protagonist.parse(api, function (err, result) {
+      if (err) {
+        grunt.fail.warn(err);
+        return;
+      }
+
+      if (result.warnings.length > 0) {
+        var warnings = '';
+        result.warnings.forEach(function (item, i, a) {
+          item.location.forEach(function (loc, k, b) {
+            warnings += 'line:' + api.substr(0, loc.index).split('\n').length + ' - ' + item.message + "\n";
+          });
         });
-      });
-      grunt.fail.warn(warnings);
-    }
+        grunt.fail.warn(warnings);
+      }
+      done();
+    });
+
   }
 
   grunt.registerMultiTask('protagonist', 'API Blueprint parser', function() {
     if (!this.data) { return false; }
 
     var data = this.data,
-        file = null;
+        file = null,
+        done = this.async();
 
     // we need to determine if this is a string or an array
     if (typeof(data) === 'string' && (data instanceof Array === false)) {
       // string
-      parseAPI(data);
+      parseAPI(data, done);
     } else if (data instanceof Array === true){
       // array, loop through it
       for (var i = 0; i < data.length; i++) {
         file = data[i];
-        parseAPI(file);
+        parseAPI(file, done);
       }
     } else {
       // something else, throw an error
@@ -58,6 +67,7 @@ module.exports = function(grunt) {
       grunt.log.writeln("        api : 'path/to/api',");
       grunt.log.writeln("        apis : [ 'path/to/apiTwo', 'path/to/apiThree' ]");
       grunt.log.writeln("    }");
+      done();
     }
   });
 
